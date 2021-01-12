c3d44c3a1cf3a8aa3efccbbec4c09c6e8d9262dd
sakamot <matuhana.8@gmail.com>
2021-01-12 17:57:53 +0900

    :bug: fix rspec

--- spec/abc188/d_snuke_prime_spec.rb
+++ spec/abc188/d_snuke_prime_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../../abc188/d_snuke_prime'
 
-describe 'water_heater' do
+describe 'snuke_prime' do
   it 'case1' do
     allow(ARGF).to receive(:gets).and_return("2 6\\n", "1 2 4\\n", "2 2 4\\n")
 
-    expect(water_heater).to eq 10
+    expect(snuke_prime).to eq 10
   end
 
   it 'case2' do
@@ -12,7 +12,7 @@ describe 'water_heater' do
                                              "136809000 653199778 90962\\n", "54601291 785892285 50554\\n",
                                              "5797762 453599267 65697\\n", "468677897 916692569 87409\\n")
 
-    expect(water_heater).to eq 163089627821228
+    expect(snuke_prime).to eq 163089627821228
   end
 
   it 'case3' do
@@ -20,6 +20,6 @@ describe 'water_heater' do
                                              "136809000 653199778 90962\\n", "54601291 785892285 50554\\n",
                                              "5797762 453599267 65697\\n", "468677897 916692569 87409\\n")
 
-    expect(water_heater).to eq 88206004785464
+    expect(snuke_prime).to eq 88206004785464
   end
 end
